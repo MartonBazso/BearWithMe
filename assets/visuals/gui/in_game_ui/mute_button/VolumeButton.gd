6384b917af34fbecef51b6c818a54c8cf484bf7f
bazsomarci98 <bazsomarci98@gmail.com>
2019-11-21 22:10:47 +0100

    Fixed volume

--- assets/visuals/gui/in_game_ui/mute_button/VolumeButton.gd
+++ assets/visuals/gui/in_game_ui/mute_button/VolumeButton.gd
@@ -7,5 +7,8 @@ func _ready():
 
 
 func _on_VolumeButton_toggled(button_pressed):
-	MusicPlayer.stream_paused = globals.isVolumeOn
+	if !globals.isVolumeOn:
+		MusicPlayer.volume_db = -40
+	else:
+		MusicPlayer.volume_db = -100
 	globals.isVolumeOn = !button_pressed
