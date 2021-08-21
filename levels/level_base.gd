bfb0dd9baedce3720b273c5bbfbab43257dd2960
bazsomarci98 <bazsomarci98@gmail.com>
2021-08-21 16:45:31 +0200

    implement proper lighting and camera work test

--- levels/level_base.gd
+++ levels/level_base.gd
@@ -5,9 +5,5 @@ extends Node2D
 export(int) var level_number = 0
 
 func _ready():
-	var viewport = get_tree().get_root()
-	var size = 128
-	#viewport.size = Vector2(size,size*1920/1080)
-	print("stop")
-
+	pass
 
